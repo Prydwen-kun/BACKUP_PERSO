1d1808c3666c028c47c0528749fc7c8da2ed4e29
Prydwen-kun <118287298+Prydwen-kun@users.noreply.github.com>
2024-04-30 12:52:46 +0200

    TestBurgerMenu
    Simple test for a burger menu that open with top button and close when clicking on main content.

---
+++ FORMATION/TP/MenuBurgerTest/assets/js/script.js
@@ -0,0 +1,19 @@
+function menuPop() {
+  console.log("menuPop has been called");
+  let x = document.getElementById("navMenu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function menuClose() {
+  console.log("menuClose has been called");
+  let x = document.getElementsByClassName("topNav");
+  console.log(x);
+  if (x[0].style.display === "block") {
+    x[0].style.display = "none";
+  }
+}
+console.log("Debug Console work as intended")
