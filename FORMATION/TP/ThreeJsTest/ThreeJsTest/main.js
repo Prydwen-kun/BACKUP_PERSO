4121f776b57e9b24b3119c5e2725d6e71cdd1788
Prydwen-kun <118287298+Prydwen-kun@users.noreply.github.com>
2024-08-30 16:12:45 +0200

    Update main.js

--- FORMATION/TP/ThreeJsTest/ThreeJsTest/main.js
+++ FORMATION/TP/ThreeJsTest/ThreeJsTest/main.js
@@ -1,17 +1,21 @@
-import './style.css'
+import "./style.css";
 // import javascriptLogo from './javascript.svg'
 // import viteLogo from '/vite.svg'
-import * as PLAYER from './player.js'
-import * as MOBS from './ClassModules/mobs.js'
-import * as THREE from './three.js-master/build/three.module.js';
-import * as MAP from './ClassModules/map.js';
-import * as CANNON from 'cannon-es'
-import * as CANNON_INIT from './ClassModules/cannon_init.js';
-
+import * as PLAYER from "./player.js";
+import * as MOBS from "./ClassModules/mobs.js";
+import * as THREE from "./three.js-master/build/three.module.js";
+import * as MAP from "./ClassModules/map.js";
+import * as CANNON from "cannon-es";
+import * as CANNON_INIT from "./ClassModules/cannon_init.js";
 
 //INIT SCENE AND CAMERA
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+  70,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 scene.background = new THREE.Color(0xfa6f66);
 scene.fog = new THREE.Fog(0xbbbbbb, 10, 500);
 scene.updateWorldMatrix(true, true);
@@ -30,8 +34,6 @@ const loader = new THREE.TextureLoader();
 
 //CLOCK
 const clock = new THREE.Clock();
-
-
 
 // ROTATING CUBE
 const geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -90,32 +92,24 @@ mob2.addToScene(scene);
 // scene.remove(object); USE THIS FOR UPDATING KILL EVENT
 ///////////////
 // EVENT LISTENERS AND PAUSE MENU
-const blocker = document.getElementById('blocker');
-const instructions = document.getElementById('instructions');
+const blocker = document.getElementById("blocker");
+const instructions = document.getElementById("instructions");
 
-instructions.addEventListener('click', function () {
-
+instructions.addEventListener("click", function () {
   player1.getPlayerControls().lock();
-
 });
 
-player1.getPlayerControls().addEventListener('lock', function () {
-
-  instructions.style.display = 'none';
-  blocker.style.display = 'none';
-
+player1.getPlayerControls().addEventListener("lock", function () {
+  instructions.style.display = "none";
+  blocker.style.display = "none";
 });
 
-player1.getPlayerControls().addEventListener('unlock', function () {
-
-  blocker.style.display = 'block';
-  instructions.style.display = '';
-
+player1.getPlayerControls().addEventListener("unlock", function () {
+  blocker.style.display = "block";
+  instructions.style.display = "";
 });
 
 scene.add(player1.getPlayerControls().getObject());
-
-
 
 ////////////////////SOME RANDOM TEST SPHERE///////////////////////
 const geometrySphere = new THREE.SphereGeometry(1, 32, 32);
@@ -140,7 +134,7 @@ CANNON_INIT.addSphereCollider(sphere1Collider, world, sceneObjectArray);
 
 //DELTA SUM DEBUG
 let deltaSum = 0;
-console.log('scene object array : ', sceneObjectArray);
+console.log("scene object array : ", sceneObjectArray);
 /////////////////////////////////////////////////APP MAIN LOOP////////////////////////////////////
 /////////////////////////////////////////////////APP MAIN LOOP////////////////////////////////////
 /////////////////////////////////////////////////APP MAIN LOOP////////////////////////////////////
@@ -151,7 +145,6 @@ function updatePlay() {
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
 
-
   //UPDATE ALL ACTOR IN THE SCENE
   player1.update(deltaTimeStoring, map1.getFloorObject());
   mob1.update(player1.mesh.position, deltaTimeStoring);
@@ -161,7 +154,10 @@ function updatePlay() {
   deltaSum += deltaTimeStoring;
 
   if (deltaSum > 2) {
-    sphere1Collider.body.applyImpulse(new CANNON.Vec3(0, 80, 0), sphere1Collider.mesh.position);
+    sphere1Collider.body.applyImpulse(
+      new CANNON.Vec3(0, 80, 0),
+      sphere1Collider.mesh.position
+    );
     deltaSum = 0;
   }
 
@@ -175,18 +171,18 @@ updatePlay();
 //IN CASE OF WINDOW RESIZE//
 onresize = (event) => {
   renderer.setSize(window.innerWidth * 0.8, window.innerHeight * 0.8);
-  camera.aspect = (window.innerWidth / window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
 };
 /////////////////////////////////////////////////APP MAIN LOOP////////////////////////////////////
 /////////////////////////////////////////////////APP MAIN LOOP////////////////////////////////////
 /////////////////////////////////////////////////APP MAIN LOOP////////////////////////////////////
 
 //exemple de rajout html
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <nav class="nav_menu">
     <h1><a href="index.html">ThreeJs & Cannon-es Project</a></h1>
     <a href="#">Download (package)</a>
     <a href="https://github.com/Prydwen-kun/BACKUP_PERSO/tree/cannonBranch/FORMATION/TP/ThreeJsTest/ThreeJsTest">GitHub Folder</a>
     <a href="https://pmndrs.github.io/cannon-es/docs/modules.html">Cannon-es Doc</a>
   </nav>
-`
+`;
